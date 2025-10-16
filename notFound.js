61c94d7985f0befdb54c8b9b48b82691c52ea28d
Yusuf Nur <nuryusuf322@gmail.com>
2025-10-15 23:57:38 -0400

    add code to error handling middleware

---
+++ notFound.js
@@ -0,0 +1,8 @@
+// 404 Not Found middleware
+const notFound = (req, res, next) => {
+    const error = new Error(`Not found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+};
+
+module.exports = notFound;
